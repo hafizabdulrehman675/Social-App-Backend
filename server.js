7a4ca18887a86b3a994fd94a23455d1087bb8154
AbdulRehman715 <chabdulrehman715@gmail.com>
2026-04-30 17:35:24 +0500

    auto sync removed and now run db:migrate each time when we change db schema

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ sequelize
   .authenticate()
   .then(() => {
     console.log("Database connected successfully!");
-    return sequelize.sync({ alter: false });
-  })
-  .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
